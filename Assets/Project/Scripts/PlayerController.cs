7c9d29d6d733ce6ffdc74f285eb5ebf13f738212
CanGuvercin <mustafacanguvercin@gmail.com>
2025-12-22 23:01:38 +0300

    ALI CAN ATTACK NOW LIKE A STAR. PERFECT ANIMATONS ARE READY

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -1,41 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float attackCooldown = 0.3f;
+
+    private float lastAttackTime;
 
     private Rigidbody2D rb;
     private Animator animator;
 
-    // Our Input System
-    private PlayerInputActions InputActions;
+    // Input System
+    private PlayerInputActions inputActions;
     private Vector2 moveInput;
 
-    // Direction Memory for last point
+    // Last direction memory
     private Vector2 lastMoveDir = Vector2.down;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-
-        InputActions = new PlayerInputActions();
+        inputActions = new PlayerInputActions();
     }
 
     private void OnEnable()
     {
-        InputActions.Enable();
+        inputActions.Enable();
     }
 
     private void OnDisable()
     {
-        InputActions.Disable();
+        inputActions.Disable();
     }
 
     private void Update()
@@ -49,12 +48,18 @@ public class PlayerController : MonoBehaviour
         Move();
     }
 
-    private void ReadInput ()
+    private void ReadInput()
     {
-        moveInput = InputActions.Player.Move.ReadValue<Vector2>();
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+
         if (moveInput != Vector2.zero)
         {
             lastMoveDir = moveInput.normalized;
+        }
+
+        if (inputActions.Player.Attack.triggered)
+        {
+            TryAttack();
         }
     }
 
@@ -66,20 +71,25 @@ public class PlayerController : MonoBehaviour
     private void UpdateAnimator()
     {
         bool isMoving = moveInput != Vector2.zero;
-
         animator.SetBool("isMoving", isMoving);
-        Vector2 dir= isMoving ? moveInput : lastMoveDir;  
-        // if there is motion, keep input, 
-        // if there is no last direction will decide for last sprite/animation
 
-        animator.SetFloat("moveX",dir.x);
+        Vector2 dir = isMoving ? moveInput : lastMoveDir;
+
+        animator.SetFloat("moveX", dir.x);
         animator.SetFloat("moveY", dir.y);
-
     }
 
+    private void TryAttack()
+    {
+        if (Time.time - lastAttackTime < attackCooldown)
+            return;
 
+        lastAttackTime = Time.time;
 
+        // Attack yönünü son bakılan yöne kilitle
+        animator.SetFloat("moveX", lastMoveDir.x);
+        animator.SetFloat("moveY", lastMoveDir.y);
 
-
-
+        animator.SetTrigger("attack");
+    }
 }
